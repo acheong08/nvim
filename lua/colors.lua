268b1fa50be8574430839a6c86c121cba7c625d2
Antonio Cheong <acheong@duti.dev>
2025-10-31 16:52:00 +0000

    onedark

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,2 +1,10 @@
+vim.pack.add({ { src = "https://github.com/navarasu/onedark.nvim" } })
+
+-- Colors
+require("onedark").setup({
+	style = "deep",
+})
+-- Enable theme
+require("onedark").load()
 vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
 vim.cmd("highlight EndOfBuffer ctermbg=NONE guibg=NONE")
