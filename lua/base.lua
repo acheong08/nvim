12bb6e96a0c44fdaff691039774b26641999d6e1
Antonio Cheong <acheong@duti.dev>
2026-01-30 13:47:49 +0000

    Relative numbers off because I dont use them anyways

--- lua/base.lua
+++ lua/base.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
 vim.opt.clipboard = "unnamedplus"
