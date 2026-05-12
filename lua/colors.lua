41931117dc97903cb738d3b70eaef17a477bd4e6
Antonio Cheong <acheong@duti.dev>
2026-05-12 02:58:46 +0100

    transparent nvim cuz i fixed foot

--- lua/colors.lua
+++ lua/colors.lua
@@ -9,5 +9,5 @@ require("onedark").setup({
 })
 -- Enable theme
 require("onedark").load()
--- vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
--- vim.cmd("highlight EndOfBuffer ctermbg=NONE guibg=NONE")
+vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
+vim.cmd("highlight EndOfBuffer ctermbg=NONE guibg=NONE")
